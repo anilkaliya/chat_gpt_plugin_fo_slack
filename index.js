39a6a50a4bcfc841d88c9324a078cc367cebb477
Shilpee Sinha <shilpeesinha@Shilpees-MBP.domain.name>
2023-06-30 18:25:50 +0530

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const axios = require('axios');
-
- const handler = async(event) => {
-    console.log(event)
-
-    
-    const OPEN_AI_KEY='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'
-    
-    const SLACK_TOKEN="xoxp-5518079832001-5502516903573-5509288687412-9e3d895da58d37d59c1ce572b5da3600"
-    const CHANNEL_ID="C05FKAY5AE4"
+ exports.handler = async(event) => {
+    const item=event.event.item
+    const question=item.message.text
+    const OPEN_AI_KEY=process.env.OPEN_AI_KEY
+    const SLACK_TOKEN=process.env.SLACK_TOKEN
+    const CHANNEL_ID=process.env.CHANNEL_ID
     const slackPrefix="https://slack.com/api"
     const COMPLETION_API_PREFIX='https://api.openai.com/v1/engines/text-davinci-002/completions'
     const slackApi=`${slackPrefix}/chat.postMessage`
     const completion_body={
-        prompt: "what is rainbow",
+        prompt: question,
         max_tokens: 50,
         temperature: 0.7,
         n: 1
@@ -31,6 +28,4 @@ const axios = require('axios');
           Authorization: `Bearer ${SLACK_TOKEN}`
         }
       })
-      console.log(res)
 };
-handler({})
